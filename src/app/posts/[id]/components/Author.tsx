e29952a60e3ec819c597695e63dd52042e0ffbae
Luca <luca.mqf@gmail.com>
2025-05-21 21:26:42 -0300

    feat: add link to author page

--- src/app/posts/[id]/components/Author.tsx
+++ src/app/posts/[id]/components/Author.tsx
@@ -4,6 +4,7 @@ import { getUser } from "@/actions/users";
 import { InfoSection } from "@/app/user/[id]/components/InfoSection";
 import { Avatar } from "@/components/ui/avatar";
 import * as E from "@/utils/either";
+import Link from "next/link";
 
 interface IProps {
   userId: number;
@@ -18,26 +19,28 @@ export async function Author({ userId }: IProps) {
   const { name, username, email } = userResult.value;
 
   return (
-    <section className="h-28 border rounded-lg border-gray-200 p-4 flex items-center gap-4 bg-white">
-      <Avatar
-        className="h-14 w-14"
-        initialClassName="bg-primary-400 text-neutral-800 text-xl font-medium"
-        initials={userResult.value.name
-          .split(" ")
-          .map((name) => name.charAt(0))
-          .join("")}
-      />
-
-      <div className="space-y-1">
-        <h1 className="text-md font-bold truncate">{name}</h1>
-
-        <InfoSection
-          items={[
-            { text: username, icon: User },
-            { text: email, icon: Mail },
-          ]}
+    <Link href={`/user/${userId}`}>
+      <section className="h-28 border rounded-lg border-gray-200 p-4 flex items-center gap-4 bg-white">
+        <Avatar
+          className="h-14 w-14"
+          initialClassName="bg-primary-400 text-neutral-800 text-xl font-medium"
+          initials={userResult.value.name
+            .split(" ")
+            .map((name) => name.charAt(0))
+            .join("")}
         />
-      </div>
-    </section>
+
+        <div className="space-y-1">
+          <h1 className="text-md font-bold truncate">{name}</h1>
+
+          <InfoSection
+            items={[
+              { text: username, icon: User },
+              { text: email, icon: Mail },
+            ]}
+          />
+        </div>
+      </section>
+    </Link>
   );
 }
